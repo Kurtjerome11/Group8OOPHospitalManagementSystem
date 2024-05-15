05d03cf0eda29ee2be56db5db6def70133f2f88a
markneo-escobarte <164448782+markneo-escobarte@users.noreply.github.com>
2024-05-16 01:31:49 +0800

    Mark Neo F. Escobarte ReportUI

--- src/hospitalmanagement/ReportUI.java
+++ src/hospitalmanagement/ReportUI.java
@@ -14,21 +14,21 @@ public class ReportUI{
     ReportUI() {
         // Frame settings
         f1.setSize(1200, 700);
-        f1.getContentPane().setBackground(Color.BLACK);
+        f1.getContentPane().setBackground(Color.decode("#94c2e1"));
         
         // Label settings
-        l1 = new JLabel("Reports");
-        l1.setBounds(40, 0, 1000, 100);
-        l1.setFont(new Font("Cambria",Font.PLAIN,35));
-        l1.setForeground(Color.WHITE);
+        l1 = new JLabel("HOSPITAL REPORT");
+        l1.setBounds(370, 0, 1090, 100);
+        l1.setFont(new Font("Open Sans",Font.BOLD,50));
+        l1.setForeground(Color.decode("#042757"));
         
         l4 = new JLabel("______________________________________________________________________________");
         l4.setBounds(0, 30, 2000, 100);
         l4.setFont(new Font("Arial",Font.PLAIN,30));
         l4.setForeground(Color.WHITE);
         
-        l5 = new JLabel("______________________________________________________________________________");
-        l5.setBounds(0, 510, 2000, 100);
+        l5 = new JLabel("");
+        l5.setBounds(0, 580, 2000, 100);
         l5.setFont(new Font("Arial",Font.PLAIN,30));
         l5.setForeground(Color.WHITE);
         
@@ -37,8 +37,8 @@ public class ReportUI{
         
        
         //code for load image
-        ImageIcon i1 = new ImageIcon("C:\\Users\\Mhacee Caryl\\Desktop\\bubuy\\.jpg");
-        Image i2 = i1.getImage().getScaledInstance(1200, 480, Image.SCALE_SMOOTH);
+        ImageIcon i1 = new ImageIcon("Images/report image.jpg");
+        Image i2 = i1.getImage().getScaledInstance(1196, 570, Image.SCALE_SMOOTH);
         ImageIcon i3 = new ImageIcon(i2);
         
         //label for the pic
@@ -47,21 +47,23 @@ public class ReportUI{
         //panel for image on the ui and it setttings
         p1 = new JPanel();
         p1.setBounds(-150, 90, 1500, 700);
-        p1.setBackground(Color.BLACK);
+        p1.setBackground(Color.WHITE);
         
         //will add the label to the panel
         p1.add(limg);
         
         //button that goes back to the dashboard
         b1 = new JButton("Back");
-        b1.setBounds(1060, 600, 80, 30);
+        b1.setBounds(1060, 620, 80, 30);
+        b1.setForeground(Color.black);
         b1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 f1.dispose();
                 new MainUi();
-            }
+            }  
         });
         
+   
         // Add objects to the frame
         f1.add(l1);
         f1.add(l4);

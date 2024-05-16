3f7fb6d94d77aeaf19da9d89e2fc6e8956fd05e8
Oliva, Kurt Jerome M. <165077111+Kurtjerome11@users.noreply.github.com>
2024-05-17 02:11:53 +0800

    final (Kurt Jerome Oliva)

--- src/hospitalmanagement/ManageUI.java
+++ src/hospitalmanagement/ManageUI.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
-
 public class ManageUI{
     private JFrame f1 = new JFrame("Group8 OOP");
     private JLabel l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, limg;
